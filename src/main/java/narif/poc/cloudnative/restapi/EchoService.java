04f3e2f131dd6dd4b6793a31f60c4e5bc50c753a
Najeeb Arif <NajeebArif786@outlook.com>
2020-05-28 23:44:22 +0530

    Update EchoService.java

--- src/main/java/narif/poc/cloudnative/restapi/EchoService.java
+++ src/main/java/narif/poc/cloudnative/restapi/EchoService.java
@@ -2,12 +2,13 @@ package narif.poc.cloudnative.restapi;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.time.LocalDateTime;
 
 @Path("ping")
 public class EchoService {
 
     @GET
     public String ping(){
-        return "PONG";
+        return "PONG: "+LocalDateTime.now();
     }
 }
